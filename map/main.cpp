a2a54c655bb09b6a1469c26d9355c121fc69c6a1
Donato Lanzilotti <dlanzilo@e3r4p3.42roma.it>
2022-03-26 19:46:24 +0100

    a

--- map/main.cpp
+++ map/main.cpp
@@ -9,14 +9,14 @@ int main()
     map<string, string> uomo;
    
 
-   uomo.insert(pair<string, string>("carlone", "cuoco"));
+    uomo.insert(pair<string, string>("carlone", "cuoco"));
 
     map<string, string>::iterator itr;
 
-    uomo["carletto"];
     for (itr = uomo.begin() ;itr != uomo.end();itr++)
     {
        cout << itr->first << "-" << itr->second <<  endl;
     }
+    //continuare a vedere tree (gli alberi lezioni su youtube), documentazione ecc...
     
 }
