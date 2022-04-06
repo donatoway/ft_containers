e0cefcdec0c34ee5ef058709b0516aef95c8a38a
donatoway <donatoway@gmail.com>
2022-04-07 01:03:32 +0200

    jj

--- map/main.cpp
+++ map/main.cpp
@@ -3,61 +3,7 @@
 #include <algorithm>
  #include "RBtree.hpp"
 #include "pair.hpp"
-
-namespace ft
-{
-    template<class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<Node<ft::pair<Key, T> > > >
-    class map
-    {
-        public:
-                typedef                 Key                         key_type;
-                typedef                 T                           mapped_type;
-                typedef      typename   ft::pair<Key, T>            value_type;
-                typedef      typename   std::size_t                 size_type;
-                typedef                 Allocator                   allocator_type;
-                typedef      typename   allocator_type::pointer     pointer;
-                typedef      typename   ft::RBTree<value_type>     tree;
-        private:
-                tree                    _tree;
-                pointer                 _map;
-                allocator_type          _allocation;
-                size_type               _size;
-        public:
-                // costrutore di base
-                map() :_size(0), _map(NULL) {};
-                //
-                explicit map( const Compare& comp,  const Allocator& alloc = Allocator() ) :  _allocation(alloc)
-                {
-                    _allocation.allocate(0);
-                    _size = 0;
-                }
-                void print(Node<value_type> *_map)
-                {
-                    if (_map == NULL)
-                    return;
-            
-                    print(_map->left);
-                    std::cout << _map->data.first <<  " " << _map->data.second << "\n";
-                    print(_map->right);
-                    
-                }
-
-                void print2(){print(_map);};
-                void insert( const value_type& value )
-                {
-                    _size++;
-                    _tree.insert(value);
-                    _map =_tree.root;
-                }
-
-                ~map()
-                {
-                  
-                }
-    };
-}
-// Driver Code
-#include <map>
+#include "map.hpp"
 
 int main()
 {
@@ -82,18 +28,10 @@ int main()
   
 
       m.print2();
- //   tree.insert(pair_val);
- //   tree.insert(pair_val2);
-
- //   cout << "Inorder Traversal of Created Tree\n";
-  //  tree.inorder();
- 
-  //  cout << "\n\nLevel Order Traversal of Created Tree\n";
-   // tree.levelOrder();
- 
 
 
+    //continuare a costruire i costruttori di map
 
-    //completare la classe 
+    
     return 0;
 }
--- map/map.hpp
+++ map/map.hpp
@@ -1,33 +1,63 @@
 #ifndef MAP_HPP
 #define MAP_HPP
+
 #include <iostream>
-#include <memory>
-#include "RBtree.hpp"
 #include "pair.hpp"
+#include "RBtree.hpp"
 
 
-/*
-    Map riceve 2 parametri: sostanzialmente il primo definisce Il tipo di Chiave (esempio int, char, string ecc.)
-    il secondo parametro definisce il Tipo del valore associato a quella chiave 
-
-
-*/
 namespace ft
-{      /* Tipo della chiave - Mapped value - key_compare - allocazione allocator_type */
-    template < class Key,  class T,  class Compare = std::less<Key>, class Alloc = std::allocator<ft::pair<const Key,T> > >
+{
+    template<class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<Node<ft::pair<Key, T> > > >
     class map
     {
-        typedef typename    Key                                     key_type;
-        typedef typename    T                                       mapped_type;
-        typedef typename    std::pair<key_type, mapped_type>        value_type;
-        typedef typename    std::less<key_type>                     key_compare;
-        //
+        public:
+                typedef                 Key                         key_type;
+                typedef                 T                           mapped_type;
+                typedef      typename   ft::pair<Key, T>            value_type;
+                typedef      typename   std::size_t                 size_type;
+                typedef                 Allocator                   allocator_type;
+                typedef      typename   allocator_type::pointer     pointer;
+                typedef      typename   ft::RBTree<value_type>     tree;
+        private:
+                tree                    _tree;
+                pointer                 _map;
+                allocator_type          _allocation;
+                size_type               _size;
+        public:
+                // costrutore di base
+                map() :_size(0), _map(NULL) {};
+                //
+                explicit map( const Compare& comp,  const Allocator& alloc = Allocator() ) :  _allocation(alloc)
+                {
+                    _allocation.allocate(0);
+                    _size = 0;
+                }
+                void print(Node<value_type> *_map)
+                {
+                    if (_map == NULL)
+                    return;
+            
+                    print(_map->left);
+                    std::cout << _map->data.first <<  " " << _map->data.second << "\n";
+                    print(_map->right);
+                    
+                }
 
-    };  
+                void print2(){print(_map);};
+                
+                void insert( const value_type& value )
+                {
+                    _size++;
+                    _tree.insert(value);
+                    _map =_tree.root;
+                }
 
-
-
-
+                ~map()
+                {
+                  
+                }
+    };
 }
 
 #endif
